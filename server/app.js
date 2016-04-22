5486822347d035d9525cc31abbf564ba58635964
Mike DuVall <mike.a.duvall@gmail.com>
2016-04-22 07:19:23 -0500

    Inital adding of Stormpath'

--- server/app.js
+++ server/app.js
@@ -9,16 +9,42 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
+var ExpressStormpath = require('express-stormpath');
+
 // Setup server
+
+// var path = require('path');
 var app = express();
+
 var server = require('http').createServer(app);
+
+
 require('./config/express')(app);
+
+var path = require('path');
+
+app.use(ExpressStormpath.init(app, {
+  website: true,
+  web: {
+    spaRoot: path.join(__dirname, '..','client','index.html')
+  }
+}));
+
+
+
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+app.on('stormpath.ready', function() {
+  // Start server
+  server.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
 });
+
+// server.listen(config.port, config.ip, function () {
+//   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+// });
 
 // Expose app
 exports = module.exports = app;
