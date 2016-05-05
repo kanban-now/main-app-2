991074c31c7f88d5c252b78b8e5141921d545257
Mike DuVall <mike.a.duvall@gmail.com>
2016-05-05 07:27:40 -0500

    Tweaked main page to be only Kanban board.  Added extra column

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -10,13 +10,14 @@ angular.module('dashboardApp')
 
     $scope.models = {
       selected: null,
-      lists: {"A": [], "B": []}
+      lists: {"A": [], "B": [], "C": []}
     };
 
     // Generate initial model
     for (var i = 1; i <= 3; ++i) {
       $scope.models.lists.A.push({label: "Item A" + i});
       $scope.models.lists.B.push({label: "Item B" + i});
+      $scope.models.lists.C.push({label: "Item C" + i});
     }
 
     // Model to JSON for demo purpose
