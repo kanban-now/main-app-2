224f6feb93184979e16d5a83f4e9fe122f0f1d9e
Mike DuVall <mike.a.duvall@gmail.com>
2016-05-18 07:40:52 -0500

    Updating logging on drag events and hardcoded kanban board layout in prep for adding backend callbacks

--- client/app/advanced/advanced.controller.js
+++ client/app/advanced/advanced.controller.js
@@ -1,16 +1,30 @@
 'use strict';
 
 angular.module('dashboardApp')
-  .controller('AdvancedCtrl', function ($scope) {
+  .controller('AdvancedCtrl', function ($scope, $http) {
 
-      $scope.dragoverCallback = function(event, index, external, type) {
-          $scope.logListEvent('dragged over', event, index, external, type);
-          // Disallow dropping in the third row. Could also be done with dnd-disable-if.
-          return index < 10;
+
+      $scope.awesomeThings = [];
+
+      $http.get('/api/things').success(function(awesomeThings) {
+          $scope.awesomeThings = awesomeThings;
+      });
+
+
+      $scope.dragoverCallback = function(event, position, external, type) {
+//          $scope.logListEvent('dragged over', event, index, external, type);
+          var message = 'event:' + event + ',position:' + position + ', external:' + external + ',type:' + type;
+          console.log(message);
+          // // Disallow dropping in the third row. Could also be done with dnd-disable-if.
+          // return position < 10;
+          return true;
       };
 
-      $scope.dropCallback = function(event, index, item, external, type, allowedType) {
-          $scope.logListEvent('dropped at', event, index, external, type);
+      $scope.dropCallback = function(event, position, item, external, type, allowedType) {
+          // $scope.logListEvent('dropped at', event, index, external, type);
+          var message = 'event:' + event + ',position:' + position + ',item:' + item + ', external:' + external + ',type:' + type + ',allowedType:' + allowedType;
+          console.log(message);
+
           if (external) {
               if (allowedType === 'itemType' && !item.label) return false;
               if (allowedType === 'containerType' && !angular.isArray(item)) return false;
@@ -18,16 +32,16 @@ angular.module('dashboardApp')
           return item;
       };
 
-      $scope.logEvent = function(message, event) {
-          console.log(message, '(triggered by the following', event.type, 'event)');
-          console.log(event);
-      };
-
-      $scope.logListEvent = function(action, event, index, external, type) {
-          var message = external ? 'External ' : '';
-          message += type + ' element is ' + action + ' position ' + index;
-          $scope.logEvent(message, event);
-      };
+      // $scope.logEvent = function(message, event) {
+      //     console.log('message:', message, '(triggered by the following', event.type, 'event)');
+      //     console.log('event:', event);
+      // };
+      //
+      // $scope.logListEvent = function(action, event, index, external, type) {
+      //     var message = external ? 'External ' : '';
+      //     message += type + ' element is ' + action + ' position ' + index;
+      //     $scope.logEvent(message, event);
+      // };
 
       $scope.model = [];
 
