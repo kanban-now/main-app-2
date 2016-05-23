b50b20eb9920db4553f6a2c77f724665ca216486
Mike DuVall <mike.a.duvall@gmail.com>
2016-05-23 07:38:35 -0500

    Added support for moving card from location 2 to location 1

--- server/api/cards/cards.controller.js
+++ server/api/cards/cards.controller.js
@@ -11,34 +11,64 @@
 
 var _ = require('lodash');
 
-
-var location1Cards = [
+var cardList = [
   {
     cardId: 1,
     cardText: 'TL 1',
+    location: 1
   }, {
     cardId: 2,
     cardText: 'TL 2',
+    location: 1
   }, {
     cardId: 3,
     cardText: 'TL 3x',
-  }
-];
-
-
-var location2Cards = [
+    location: 1
+  },
   {
     cardId: 4,
     cardText: 'TM 1',
+    location: 2
   }, {
     cardId: 5,
     cardText: 'TM 2',
+    location: 2
   }, {
     cardId: 6,
     cardText: 'TM 3',
+    location: 2
   }
+
 ];
 
+
+
+function getCardsAtLocation(locationId) {
+  var cardsAtLocation = [];
+
+  var arrayLength = cardList.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var nextItem = cardList[i]
+    if( nextItem.location === locationId) {
+      cardsAtLocation.push(nextItem);
+    }
+  }
+  return cardsAtLocation;
+}
+
+function getCardWithId(cardId) {
+
+  var arrayLength = cardList.length;
+  for (var i = 0; i < arrayLength; i++) {
+    var nextItem = cardList[i]
+    if( nextItem.cardId  === cardId) {
+      return nextItem;
+    }
+  }
+
+  return null;
+
+}
 
 
 // Get list of things
@@ -47,10 +77,24 @@ exports.index = function(req, res) {
   var location = req.query.location;
   console.log('location=' + location);
   if (location === '1') {
-    res.json(location1Cards);
+    var cardsAtLocation = getCardsAtLocation(1);
+    res.json(cardsAtLocation);
   }
   if (location === '2') {
-    res.json(location2Cards);
+    var cardsAtLocation = getCardsAtLocation(2);
+    res.json(cardsAtLocation);
   }
+
+};
+
+
+exports.handleMove = function(req, res) {
+
+  var cardIdAsInt = parseInt(req.params.cardId);
+  var newDestinationAsInt = parseInt(req.params.destination);
+  var foundCard = getCardWithId(cardIdAsInt);
+  foundCard.location = newDestinationAsInt;
+  var myLocalCardList = cardList;
+  console.log('test');
 
 };
--- server/api/cards/index.js
+++ server/api/cards/index.js
@@ -7,9 +7,6 @@ var router = express.Router();
 
 router.get('/', controller.index);
 
-// LEFT OFF - figure out how to make below work to handle a move.
-// Read up on Express docs for how to do complex URL like below
-//var url = '/api/cards/' + item.cardId + '/move/' + destination;
-router.post('/{cardId}/move/{destination', controller.handleMove);
+router.post('/:cardId/move/:destination', controller.handleMove);
 
 module.exports = router;
